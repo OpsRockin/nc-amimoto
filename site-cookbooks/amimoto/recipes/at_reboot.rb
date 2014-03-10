bf39b2a026b31301fa70a837df2187c60b8de9a3
sawanoboly <sawanoboriyu@higanworks.com>
2014-03-10 15:38:14 +0900

    forget username

--- site-cookbooks/amimoto/recipes/at_reboot.rb
+++ site-cookbooks/amimoto/recipes/at_reboot.rb
@@ -1,6 +1,6 @@
 ruby_block 'at_reboot' do
   block {
-    reboot_line = '@reboot /usr/bin/curl https://raw.github.com/megumiteam/amimoto/master/initial.centos | /bin/bash'
+    reboot_line = '@reboot root /usr/bin/curl https://raw.github.com/megumiteam/amimoto/master/initial.centos | /bin/bash'
     _file = Chef::Util::FileEdit.new('/etc/crontab')
     _file.insert_line_if_no_match(reboot_line,reboot_line)
     _file.write_file
